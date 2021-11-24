aa0db3593ced0b7e131f06cecec51252b11d2dd0
Jongwan Kim <yena2bell@kaist.ac.kr>
2021-11-24 22:01:17 +0900

    make function to have a same interface function of parse.py. also make expanded_model class and common ancestor class between expanded model class and net class

--- parse_with_class.py
+++ parse_with_class.py
@@ -8,6 +8,11 @@ import os, sys, yaml, util, math
 
 CUPY, cp = util.import_cp_or_np(try_cupy=0) #should import numpy as cp if cupy not installed
 
+def params(param_file):
+    param_object = Params(param_file)
+    
+    return param_object.params
+
 class Params:
     def __init__(self, params_file):
         self.param_file = params_file
@@ -64,35 +69,122 @@ class Params:
             # TODO: should be a multiple of parallellism, not nec ==
             # see also basin.get_init_sample()
             self.params['num_samples'] = self.params['parallelism'] = actual_num_samples
+
+def net(params):
+    net_file = params["net_file"]
+    debug = params['debug']
+    clause_bin_size = params["clause_bin_size"]
+    parallelism = params['parallelism']
     
-    def get_logic(self):
-        # returns F, V
-        # F: the logic of the network, where F={}, F[node_name] = [clause1,clause2,...] and each clause=[literal1,literal2,...]
-        #	DNF so F[node] = clause1 OR clause2 ..., whereas clause = literal1 AND literal2 ...
-        # V['#2name'] = [] that maps node number's to their original names
-        # V['name2#'] = {} that maps node's name to their number
-        pass
+    net_object = Net(net_file, debug, clause_bin_size, parallelism)
+    net_object.apply_mutations(params)
+    net_object.get_clause_mapping()
     
+    F = net_object.F
+    V = net_object.V
+    F_mapd = net_object.clause_mapping
+    A = net_object.A
+	# F is the logic function, and V are the nodes (specifically how node names map to numbers)
+    return F, F_mapd, A, V
     
 
-class Net:
-    def __init__(self, net_file, debug=True, clause_bin_size=99999, parallelism=128):
+class Net_reading_part:
+    def __init__(self, net_file, debug):
         self.net_file = net_file
         # net file should be in DNF, see README for specifications
         self.debug = debug
         
         self.node_name_to_num = {}
         self.node_num_to_name = []
+        
+        self.max_literals = None
+        self.max_clauses = None
+        self.num_clauses = None
+        self.n = None
+        
+        self.loop_limit = 1000000
+        
+        self.format_name = None
+        
+    def _initialize(self):
+        print("you should override this function")
+        
+    def _read_net_file(self):
+        net_file = self.net_file
+        # net file should be in DNF, see README for specifications
+        
+        if not os.path.isfile(net_file):
+            sys.exit("Can't find network file: " + str(net_file)) 
+            
+        with open(net_file,'r') as file:
+            extension = os.path.splitext(net_file)[-1]
+            if extension == '.bnet':
+                self.format_name='bnet'
+            else:
+                self.format_name = file.readline().replace('\n','')
+            self._get_file_format(self.format_name)
+            
+            loop = 0
+            while True:
+                line = file.readline()
+                if not line: #i.e eof
+                    break
+                if loop > self.loop_limit:
+                    sys.exit("Hit an infinite loop, unless net is monstrously huge")
+                self._parse_net_file_line(line)
+                loop += 1
+        
+               
+    def _parse_net_file_line(self, line):
+        line = line.strip().split(self.node_fn_split)
+        node_name = line[0].strip()
+        clauses = line[1].split(self.clause_split)
+        
+        self._process_node_name_part(node_name)
+        self._process_clauses(node_name, clauses)
+        
+    def _process_node_name_part(self, node_name):
+        node_name = self._replace_symbol_in_strip_from_node(node_name, '')
+        if self.debug:
+            assert(node_name not in self.node_name_to_num.keys())
+        
+        self.node_name_to_num[node_name] = self.n
+        self.node_num_to_name += [node_name]
+        self.n+=1
+    
+    def _process_clauses(self,node_name, clauses):
+        print("you should override it")
+    
+    def _get_file_format(self, format_name):
+        node_fn_split, clause_split, literal_split, not_str, strip_from_clause, strip_from_node = get_file_format(format_name)
+        self.node_fn_split = node_fn_split
+        self.clause_split = clause_split
+        self.literal_split = literal_split
+        self.not_str = not_str
+        self.strip_from_clause = strip_from_clause
+        self.strip_from_node = strip_from_node
+    
+    def _get_index_dtype(self):
+        if self.n<256: 
+            self.index_dtype = cp.uint8
+        elif self.n<65536:
+            self.index_dtype = cp.uint16
+        else:
+            self.index_dtype = cp.uint32
+            
+    def _replace_symbol_in_strip_from_node(self, variable, replace_value=''):
+        for symbol in self.strip_from_node:
+            variable = variable.replace(symbol,replace_value)
+        return variable
+
+
+class Net(Net_reading_part):
+    def __init__(self, net_file, debug=True, clause_bin_size=99999, parallelism=128):
+        super().__init__(net_file, debug)
         self.F= {}
         self.V = {}#{'#2name':self.node_num_to_name, 'name2#':self.node_name_to_num}
         self._initialize()
         
-        self.max_literals = 1
-        self.max_clauses = 1
-        self.num_clauses = 1
-        self.n = 1
-        
-        self.loop_limit = 1000000
         
         self._read_net_file()
         self.V = {'#2name':self.node_num_to_name, 'name2#':self.node_name_to_num}
@@ -115,68 +207,23 @@ class Net:
         self.node_name_to_num['0']=0 # always OFF node is first
         self.node_num_to_name += ['0']
         self.F['0'] = [[0]]
+        self.max_literals = 1
+        self.max_clauses = 1
+        self.num_clauses = 1
+        self.n = 1
     
     def _read_net_file(self):
-        net_file = self.net_file
-        # net file should be in DNF, see README for specifications
-        
-        if not os.path.isfile(net_file):
-            sys.exit("Can't find network file: " + str(net_file)) 
-            
-        with open(net_file,'r') as file:
-            extension = os.path.splitext(net_file)[-1]
-            if extension == '.bnet':
-                format_name='bnet'
-            else:
-                format_name = file.readline().replace('\n','')
-            self._get_file_format(format_name)
-            
-            loop = 0
-            while True:
-                line = file.readline()
-                if not line: #i.e eof
-                    break
-                if loop > self.loop_limit:
-                    sys.exit("Hit an infinite loop, unless net is monstrously huge")
-                self._parse_net_file_line(line)
-                loop += 1
-        
+        super()._read_net_file()
         self._put_negative_nodes_as_2nd_half_of_node_array()
         
         self.V['#2name'] = self.node_num_to_name
-        self.V['name2#'] = self.node_name_to_num
-                
-                
-    
-    def _get_file_format(self, format_name):
-        node_fn_split, clause_split, literal_split, not_str, strip_from_clause, strip_from_node = get_file_format(format_name)
-        self.node_fn_split = node_fn_split
-        self.clause_split = clause_split
-        self.literal_split = literal_split
-        self.not_str = not_str
-        self.strip_from_clause = strip_from_clause
-        self.strip_from_node = strip_from_node
-        
-    def _parse_net_file_line(self, line):
-        line = line.strip().split(self.node_fn_split)
-        node_name = line[0].strip()
-        clauses = line[1].split(self.clause_split)
-        
-        self._process_node_name_part(node_name)
-        self._process_clauses(node_name, clauses)
-        
+        self.V['name2#'] = self.node_name_to_num        
         
         
     def _process_node_name_part(self, node_name):
-        for symbol in self.strip_from_node:
-            node_name = node_name.replace(symbol,'')
-        if self.debug:
-            assert(node_name not in self.node_name_to_num.keys())
+        super()._process_node_name_part(node_name)
         
-        self.node_name_to_num[node_name] = self.n
-        self.node_num_to_name += [node_name]
         self.F[node_name] =  []
-        self.n+=1
     
     def _process_clauses(self,node_name, clauses):
         self.num_clauses += len(clauses)
@@ -189,8 +236,7 @@ class Net:
             literals = clause.split(self.literal_split)
             for j in range(len(literals)):
                 literal_name = literals[j]
-                for symbol in self.strip_from_node:
-                    literal_name = literal_name.replace(symbol,'')
+                literal_name = self._replace_symbol_in_strip_from_node(literal_name, '')
                 this_clause += [literal_name]
             self.F[node_name] += [this_clause]
     
@@ -211,8 +257,8 @@ class Net:
             for f in self.F:
                 if f in params['mutations']:
                     self.F[f] = [str(params['mutations'][f])] #should be '0' or '1' which refer to the always OFF and ON nodes
-                if params['debug']:
-                    assert(self.F[f][0] in ['0','1'])
+                    if params['debug']:
+                        assert(self.F[f][0] in ['0','1'])
                     
     def get_clause_mapping(self):
         if self.debug:
@@ -259,15 +305,6 @@ class Net:
             self.nodes_to_clauses[self._curr_clause] = clause_fn
             self.nodes_clause[node_num] += [self._curr_clause]
             self._curr_clause += 1
-        
-                                 
-    def _get_index_dtype(self):
-        if self.n<256: 
-            self.index_dtype = cp.uint8
-        elif self.n<65536:
-            self.index_dtype = cp.uint16
-        else:
-            self.index_dtype = cp.uint32
     
     @staticmethod
     def _get_thread_dtype(parallelism):
@@ -328,6 +365,173 @@ class Net:
         if 'mutations' in params.keys():
             for k in params['mutations']:
                 assert(k in self.V['name2#'].keys())
+
+def sequences(seq_file_pos, seq_file_neg):
+	seqs = []
+	with open(seq_file_pos,'r') as file:
+		
+		loop = 0
+		while True:
+			line = file.readline()
+			if not line: #i.e eof
+				break
+			if loop > 1000000:
+				sys.exit("Hit an infinite loop, unless file is monstrously huge") 
+
+			seq = []
+			was_tab=True
+			word=''
+			for c in line:
+				if c=='\n':
+					if word == '':
+						seq+=['']
+					else:
+						seq+=[(word,1)]
+					seqs+=[seq]
+					break
+				elif c=='\t':
+					if was_tab:
+						seq+=['']
+					else:
+						if word == '':
+							seq+=['']
+						else:
+							seq+=[(word,1)]
+					word=''
+				else:
+					was_tab=False
+					if c in ['\\','/','-']:
+						word+='_'
+					else:
+						word+=c
+
+
+
+			loop += 1
+
+
+	with open(seq_file_neg,'r') as file:
+		
+		i = 0
+		while True:
+			line = file.readline()
+			if not line: #i.e eof
+				break
+			if i > 1000000:
+				sys.exit("Hit an infinite loop, unless file is monstrously huge") 
+
+			was_tab=True
+			word=''
+			j=0
+			for c in line:
+				if c=='\n':
+					if word != '':
+						assert(seqs[i][j]=='')
+						seqs[i][j]=(word,0)
+					break
+				elif c=='\t':
+					if not was_tab and word != '':
+						assert(seqs[i][j]=='')
+						seqs[i][j]=(word,0)
+					word=''
+					j+=1
+				else:
+					was_tab=False
+					if c in ['\\','/','-']:
+						word+='_'
+					else:
+						word+=c
+			i += 1
+
+	return seqs
+
+
+class Expanded_net(Net_reading_part):
+    def __init__(self, net_file, debug):
+        super().__init__(net_file, debug)
+        self._initialize()
+        self._read_net_file()
+        
+        self.V = {}#{'#2name':self.node_num_to_name, 'name2#':self.node_name_to_num}
+        
+    def _initialize(self):
+        self.n = 0
+        self.num_clauses = 0
+        
+    def _process_clauses(self,node_name, clauses):
+        for clause in clauses:
+            literals = clause.split(self.literal_split)
+            if len(literals)>1:
+                self.num_clauses += 1
+    
+    def buile_composite_nodes_and_A(self):
+        self.N = self.num_clauses+self.n
+        self.A = cp.zeros((self.N,self.N))
+        assert(self.n%2==0) #should be even since half are regular and half are complement nodes
+
+        assert(len(self.node_name_to_num)==len(self.node_num_to_name)==self.n) #sanity check
+        self._get_index_dtype()
+        
+        self.composite_counter = 0
+        
+        self._read_second_time()
+        
+        
+    def _read_second_time(self):
+        # go thru 2nd time to build composite nodes and A
+        with open(self.net_file,'r') as file:
+            if self.format_name!='bnet':
+                line = file.readline() #again skip 1st line, the encoding
+            
+            for _ in range(self.n): 
+                line = file.readline()
+                line = line.strip().split(self.node_fn_split)
+                node = self.node_name_to_num[line[0].strip()]
+                
+                clauses = [splited.strip() for splited in line[1].split(self.clause_split)]
+                
+                # fill A with A[2*n+composite_counter,node] = 1 for each composite node
+                # and A[i,2*n+composite_counter]=1 for each input i to the composite node
+                
+                for i in range(len(clauses)):
+                    clause = clauses[i]
+                    for symbol in self.strip_from_clause:
+                        clause = clause.replace(symbol,'')
+                        
+                    literals = clause.split(self.literal_split)
+                    
+                    if len(literals)>1:
+                        self.A[self.n+self.composite_counter,node]=1
+                        for j in range(len(literals)):
+                            literal_name = literals[j]
+                            literal_name =self._replace_symbol_in_strip_from_node(literal_name, '')
+                            literal_node = self.node_name_to_num[literal_name]
+                            self.A[literal_node,self.n+self.composite_counter]=1
+                        self.composite_counter+=1
+                    else:
+                        literal_name = literals[0]
+                        literal_name =self._replace_symbol_in_strip_from_node(literal_name, '')
+                        literal_node = self.node_name_to_num[literal_name]
+                        self.A[literal_node,node]=1
+        
+        self.V = {'#2name':self.node_num_to_name, 'name2#':self.node_name_to_num}
+        if self.debug:
+            assert(self.n%2==0) #half should be negative nodes
+                        
+                            
+                
+        
+    
+    
+
+def expanded_net(params, net_file):
+    Expanded_net_object = Expanded_net(net_file, params["debug"])
+    Expanded_net_object.buile_composite_nodes_and_A()
+    A = Expanded_net_object.A
+    n = Expanded_net_object.n
+    N = Expanded_net_object.N
+    V = Expanded_net_object.V
+    return A,int(n/2),N,V
 
 
 
